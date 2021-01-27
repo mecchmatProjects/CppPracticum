33d9aa361ec7602fc4ba3675418e35e2256b5e96
Bohdan Konovalenko <60445869+Fagtoy@users.noreply.github.com>
2021-01-27 20:32:59 +0200

    Add files via upload

---
+++ dz_1_z_4.cpp
@@ -0,0 +1,27 @@
+/*Завдання 4, перша домашня робота*/
+#include <cstdio>
+
+int main(){
+    // a)
+    printf("*\n");
+    printf("***\n");
+    printf("*****\n");
+    printf("***\n");
+    printf("*\n");
+
+    // b)
+    printf("\n*****\n");
+    printf(" ** **\n");
+    printf("  *  *  *\n");
+    printf(" ** **\n");
+    printf("*****\n");
+
+    // c)
+    printf("\n*******\n");
+    printf(" *         *\n");
+    printf("  *    Hello  *\n");
+    printf(" *         *\n");
+    printf("*******\n");
+
+}
+
