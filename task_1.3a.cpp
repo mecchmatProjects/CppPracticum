5a9f3781cade236f0fa93d770139ebeafc3fb984
Lena <66275938+arc-lena@users.noreply.github.com>
2021-02-01 09:28:30 +0200

    task_1.3a

---
+++ task_1.3a.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+
+
+using namespace std;
+
+int main() {
+    float a, b;
+
+    cout << "  Type a number a:  ";
+
+    cin  >> a ;
+
+    cout << "Type a number b:  ";
+
+    cin  >> b ;
+
+    cout << "Product is " <<  a * b;
+    cout << "\n  Difference is " <<  a - b;
+
+    return 0;
+}
