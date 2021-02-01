d5ec53ddc0ce4c185eadd9a686700bf8e7b261f5
Valentina <78035300+ShiyanValentina@users.noreply.github.com>
2021-02-01 09:28:10 +0200

    Add files via upload

---
+++ Task_3.cpp
@@ -0,0 +1,23 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float a;
+    float b;
+    float c;
+    float d;
+    cout << "enter 1 number";
+    cin >> a;
+    cout << "enter 2 number";
+    cin >> b;
+    c = a - b;
+    d = a * b;
+    printf("a - b = %f\n", c);
+    printf("a * b = %f\n", d);
+
+}
+
+
