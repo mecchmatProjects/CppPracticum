ad23e9873c2daea1febd86d61ca9a3defcc7ad2f
MYKYTA BABENKO <61553734+SamiyKrasiviy@users.noreply.github.com>
2021-02-03 08:57:54 +0200

    Add files via upload

---
+++ Class_01.cpp
@@ -0,0 +1,24 @@
+﻿// Class_01.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+using namespace std;
+
+float mn(float x)
+{
+    float y = x + 1;
+    float z = 0;
+    z = y * y;
+    y = z * z * y;
+    return y;
+}
+
+
+int main()
+{
+    float x, y;
+    cin >> x;
+    y = mn(x);
+    cout << y << endl;
+}
