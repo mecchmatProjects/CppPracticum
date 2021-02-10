689d0f031fc50d7e49c08600f2b7ef1ac9f25862
MYKYTA BABENKO <61553734+SamiyKrasiviy@users.noreply.github.com>
2021-02-10 09:03:54 +0200

    Add files via upload

--- Class_01.cpp
+++ Class_01.cpp
@@ -2,23 +2,27 @@
 //
 
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
-float mn(float x)
+float pr(float x1, float y1, float x2, float y2, float x3, float y3 )
 {
-    float y = x + 1;
-    float z = 0;
-    z = y * y;
-    y = z * z * y;
+    float d1 = pow((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2), 0.5);
+    float d2 = pow((x2 - x3) * (x2 - x3) + (y2 - y3) * (y2 - y3), 0.5);
+    float d3 = pow((x1 - x3) * (x1 - x3) + (y1 - y3) * (y1 - y3), 0.5);
+    float p2 = (d1 + d2 + d3) / 2;
+    y = pow(p2 * (p2 - d1) * (p2 - d2) * (p2 - d3), 0.5);
     return y;
 }
 
 
 int main()
 {
-    float x, y;
-    cin >> x;
-    y = mn(x);
+    float x1, y1, x2, y2, x3, y3;
+    cin >> x1,y1 ;
+    cin >> x2, y2;
+    cin >> x3, y3;
+    y = pr(x1, y1, x2, y2, x3, y3);
     cout << y << endl;
 }
